2d2f10594886ef34d94abe54e85a1381b55ee8f5
shbunch <shawnbunch@gmail.com>
2023-11-03 00:58:24 -0500

    Updated header component and links to other pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Shawn</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Shawn</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -17,9 +20,19 @@ export default function Header() {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-          <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
-          <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
-          <li className="hidden sm:inline text-slate-700 hover:underline">Sign in</li>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-slate-700 hover:underline">Sign in</li>
+          </Link>
         </ul>
       </div>
     </header>
