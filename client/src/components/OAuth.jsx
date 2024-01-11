932d1eb63cc0a8c572881c69fba7b1238cb87299
shbunch <shawnbunch@gmail.com>
2024-01-10 23:31:39 -0600

    Created Firebase and Google Auth login

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
 
 export default function OAuth() {
+	const handleGoogleClick = async () => {
+		try {
+			const provider = new GoogleAuthProvider()
+			const auth = getAuth(app)
+
+			const result = await signInWithPopup(auth, provider)
+
+			const res = await fetch('/api/auth/google', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
+			})
+			const data = await res.json()
+		} catch (error) {
+			console.log('Could not sign in with gooogle', error);
+		}
+	};
 	return (
-		<button className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+		<button
+			onClick={handleGoogleClick}
+			type='button'
+			className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+		>
 			Continue with google
 		</button>
 	);
