c14390447e409e35f6b95223d75d8edafd6d8c92
shbunch <shawnbunch@gmail.com>
2024-01-12 23:52:45 -0600

    Added methods to create username from name, and default avatar

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,9 +45,10 @@ export const google = async (req, res, next) => {
 			const generatedPassword = Math.random().toString(36).slice(-8);
 			const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
 			const newUser = new User({
-				username: req.body.name,
+				username: req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4),
 				email: req.body.email,
 				password: hashedPassword,
+				avatar: req.body.photo
 			});
 		}
 	} catch (error) {
