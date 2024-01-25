4a85256272c6d9bdac0b1777aa529f40c2df3d00
shbunch <shawnbunch@gmail.com>
2024-01-24 23:30:31 -0600

    Revert "Added methods to create username from name, and default avatar"
    This reverts commit c14390447e409e35f6b95223d75d8edafd6d8c92.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,10 +45,9 @@ export const google = async (req, res, next) => {
 			const generatedPassword = Math.random().toString(36).slice(-8);
 			const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
 			const newUser = new User({
-				username: req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4),
+				username: req.body.name,
 				email: req.body.email,
 				password: hashedPassword,
-				avatar: req.body.photo
 			});
 			await newUser.save();
 			const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
