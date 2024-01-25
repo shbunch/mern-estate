b51e53f990e83cf0daf20dfe6a6925d2adc90447
shbunch <shawnbunch@gmail.com>
2024-01-24 23:33:07 -0600

    Revert "Created Firebase and Google Auth login"
    This reverts commit 932d1eb63cc0a8c572881c69fba7b1238cb87299.

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,41 +1,8 @@
 import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import { app } from '../firebase';
-import { useDispatch } from 'react-redux';
-import { signInSuccess } from '../redux/user/userSlice';
 
 export default function OAuth() {
-	const dispatch = useDispatch();
-	const handleGoogleClick = async () => {
-		try {
-			const provider = new GoogleAuthProvider();
-			const auth = getAuth(app);
-
-			const result = await signInWithPopup(auth, provider);
-
-			const res = await fetch('/api/auth/google', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					name: result.user.displayName,
-					email: result.user.email,
-					photo: result.user.photoURL,
-				}),
-			});
-			const data = await res.json();
-			dispatch(signInSuccess(data));
-		} catch (error) {
-			console.log('Could not sign in with gooogle', error);
-		}
-	};
 	return (
-		<button
-			onClick={handleGoogleClick}
-			type='button'
-			className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
-		>
+		<button className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
 			Continue with google
 		</button>
 	);
