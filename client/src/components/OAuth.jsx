3f180244fa6aeab32b74565f203f8fef87bf7cf8
shbunch <shawnbunch@gmail.com>
2024-01-29 19:36:50 -0600

    Adding provider and auth functions

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
 
 export default function OAuth() {
 	const handleGoogleClick = async () => {
 		try {
+			const provider = new GoogleAuthProvider()
+			const auth = getAuth(app)
+
+			const result = await signInWithPopup(auth, provider)
+
 		} catch (error) {
 			console.log('Could not sign in with google', error);
 		}
-	}
+	};
 	return (
-		<button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+		<button
+			onClick={handleGoogleClick}
+			type='button'
+			className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+		>
 			Continue with google
 		</button>
 	);
