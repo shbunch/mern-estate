27d15bf55dae5435538ee7e03aa5338749408e5d
shbunch <shawnbunch@gmail.com>
2024-01-29 19:07:06 -0600

    Added Firebase package and Continue with google button

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 export default function OAuth() {
+	const handleGoogleClick = async () => {
+		try {
+		} catch (error) {
+			console.log('Could not sign in with google', error);
+		}
+	}
 	return (
-		<button className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+		<button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
 			Continue with google
 		</button>
 	);
