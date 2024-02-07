158de45654388a857acaa1879f62c38720ab6e2f
shbunch <shawnbunch@gmail.com>
2024-02-06 23:52:27 -0600

    Updating header profile picture and login state

--- issues.js
+++ issues.js
@@ -4,3 +4,5 @@
 // Ran into issues that I couldn't decipher, had to familiarize myself with git revert
 
 // Video did not show how to use JWT_SECRET, had to look it up
+
+//TODO Google profile picture not showing up, currentUser.avatar on redux tab is showing default instead of the google profile picture
