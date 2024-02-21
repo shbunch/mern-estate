b92e64d14d84ee6c28d2f472efc61c1587a4e331
shbunch <shawnbunch@gmail.com>
2024-02-21 01:18:38 -0600

    Finished update user API route

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,5 +1,4 @@
-import { jwt } from 'jsonwebtoken';
-import { errorHandler } from './error';
+import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
 	const token = req.cookies.access_token;
