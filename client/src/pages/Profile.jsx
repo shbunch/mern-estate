1edf4935c6e8eb83ea9e170856902ea413f08c8e
shbunch <shawnbunch@gmail.com>
2024-02-24 23:36:41 -0600

    Added placeholder text for username and email, updated node packages

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -45,6 +45,11 @@ export default function Profile() {
 			}
 		);
 	};
+
+const handleChange = (e) => {
+	setFormData({ ...formData, [e.target.id]: e.target.value });
+}
+
 	return (
 		<div className='p-3 max-w-lg mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -80,20 +85,25 @@ export default function Profile() {
 				<input
 					type='text'
 					placeholder='username'
+					defaultValue={currentUser.username}
 					id='username'
 					className='border p-3 rounded-lg'
-				/>
+					onChange={handleChange}
+					/>
 				<input
 					type='text'
 					placeholder='email'
+					defaultValue={currentUser.email}
 					id='email'
 					className='border p-3 rounded-lg'
+					onChange={handleChange}
 				/>
 				<input
 					type='text'
 					placeholder='password'
 					id='password'
 					className='border p-3 rounded-lg'
+					onChange={handleChange}
 				/>
 				<button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
 					Update
