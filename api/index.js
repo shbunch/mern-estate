29274521f0756398c90c3dd2bed7d7ad491d66ce
shbunch <shawnbunch@gmail.com>
2024-03-07 01:31:45 -0600

    Adding delete user function

--- api/index.js
+++ api/index.js
@@ -37,3 +37,4 @@ app.use((err, req, res, next) => {
 		message,
 	});
 });
+
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
