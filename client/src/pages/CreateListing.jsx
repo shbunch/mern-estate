f3cc05ab3f4591b597c056c8400ca0253293dc0f
shbunch <shawnbunch@gmail.com>
2024-04-02 00:42:35 -0500

    Beginning process for handling image upload functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function CreateListing() {
+	const [files, setFiles] = useState([]);
+	const handleImageSubmit = (e) => {
+		e.preventDefault();
+	};
 	return (
 		<main className='p-3 max-w-4xl mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>
@@ -115,13 +119,18 @@ export default function CreateListing() {
 					</p>
 					<div className='flex gap-4'>
 						<input
+							onChange={(e) => setFiles(e.target.files)}
 							className='p-3 border border-gray-300 rounded w-full'
 							type='file'
 							id='images'
 							accept='image/*'
 							multiple
 						/>
-						<button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>
+						<button
+							type='button'
+							onClick={handleImageSubmit}
+							className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
+						>
 							Upload
 						</button>
 					</div>
