ff380c5b8dd00aa01fbc42570227fac15fb347de
shbunch <shawnbunch@gmail.com>
2024-04-09 00:52:37 -0500

    Added state variable for Upload in progress

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -13,9 +13,12 @@ export default function CreateListing() {
 		imageUrls: [],
 	});
 	const [imageUploadError, setImageUploadError] = useState(false);
+	const [uploading, setUploading] = useState(false);
 	console.log(formData);
 	const handleImageSubmit = (e) => {
 		if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+			setUploading(true);
+			setImageUploadError(false);
 			const promises = [];
 
 			for (let i = 0; i < files.length; i++) {
@@ -28,12 +31,15 @@ export default function CreateListing() {
 						imageUrls: formData.imageUrls.concat(urls),
 					});
 					setImageUploadError(false);
+					setUploading(false);
 				})
 				.catch((err) => {
-					setImageUploadError('Image upload failed (2mb max per image');
+					setImageUploadError('Image upload failed (2mb max per image)');
+					setUploading(false);
 				});
 		} else {
 			setImageUploadError('You can only upload 6 images per listing');
+			setUploading(false);
 		}
 	};
 
@@ -191,10 +197,11 @@ export default function CreateListing() {
 						/>
 						<button
 							type='button'
+							disabled={uploading}
 							onClick={handleImageSubmit}
 							className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
 						>
-							Upload
+							{uploading ? 'Uploading...' : 'Upload'}
 						</button>
 					</div>
 					<p className='text-red-700 text-sm'>
