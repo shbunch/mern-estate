450e696e80ff02dc488a70403d365bca6e3f70e7
shbunch <shawnbunch@gmail.com>
2024-05-02 00:09:26 -0500

    Built function to show listings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -29,6 +29,7 @@ export default function Profile() {
 	const [fileUploadError, setFileUploadError] = useState(false);
 	const [formData, setFormData] = useState({});
 	const [updateSuccess, setUpdateSuccess] = useState(false);
+	const [showListingsError, setShowListingsError] = useState(false);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -120,6 +121,21 @@ export default function Profile() {
 			dispatch(signOutUserFailure(data.message));
 		}
 	};
+
+	const handleShowListings = async () => {
+		try {
+			setShowListingsError(false);
+			const res = await fetch(`/api/user/listings/${currentUser._id}`);
+			const data = await res.json();
+			if (data.success === false) {
+				setShowListingsError(true);
+				return;
+			}
+		} catch (error) {
+			setShowListingsError(true);
+		}
+	}
+
 	return (
 		<div className='p-3 max-w-lg mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -209,7 +225,8 @@ export default function Profile() {
 			<p className='text-green-700 mt-5'>
 				{updateSuccess ? 'User is updated successfully!' : ''}
 			</p>
-			<button className='text-green-700 w-full'>Show Listings</button>
+			<button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+			<p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
 		</div>
 	);
 }
