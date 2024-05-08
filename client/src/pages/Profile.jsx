489e544b62f75945c70e5ca00c14051c70d0a65a
shbunch <shawnbunch@gmail.com>
2024-05-08 00:35:48 -0500

    Added header for Your Listings and buttons for deleting and editing listings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -235,30 +235,40 @@ export default function Profile() {
 				{showListingsError ? 'Error showing listings' : ''}
 			</p>
 
-			{userListings &&
-				userListings.length > 0 &&
-				userListings.map((listing) => (
-					<div
-						key={listing.id}
-						className='border rounded-lg p-3 flex justify-between items-center'
-					>
-						<Link to={`/listing/${listing._id}`}>
-							<img
-								src={listing.imageUrls[0]}
-								alt='listing cover'
-								className='h-16 w-16 object-contain'
-							/>
-						</Link>
-						<Link
-							className='text-slate-700 font-semibold flex-1 hover:underline truncate'
-							to={`/listing/${listing._id}`}
+			{userListings && userListings.length > 0 && (
+				<div className='flex flex-col gap-4'>
+					<h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+					{userListings.map((listing) => (
+						<div
+							key={listing.id}
+							className='border rounded-lg p-3 flex justify-between items-center gap-4'
 						>
-							<p>{listing.name}</p>
-						</Link>
+							<Link to={`/listing/${listing._id}`}>
+								<img
+									src={listing.imageUrls[0]}
+									alt='listing cover'
+									className='h-16 w-16 object-contain'
+								/>
+							</Link>
+							<Link
+								className='text-slate-700 font-semibold flex-1 hover:underline truncate'
+								to={`/listing/${listing._id}`}
+							>
+								<p>{listing.name}</p>
+							</Link>
 
-						<div className=''></div>
-					</div>
-				))}
+							<div className='flex flex-col item-center'>
+								<button className='text-red-700 uppercase'>
+									Delete
+								</button>
+								<button className='text-green-700 uppercase'>
+									Edit
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
