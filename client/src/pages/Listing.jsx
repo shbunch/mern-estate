5469e4d0f208f5d286d9e9929ebed69d8bb5f9db
shbunch <shawnbunch@gmail.com>
2024-05-23 00:36:55 -0500

    Creating main listing page and function to fetch listing

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Listing() {
+	const [listing, setListing] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const params = useParams();
+	useEffect(() => {
+		const fetchListing = async () => {
+			try {
+				setLoading(true)
+				const res = await fetch(`/api/listing/get/${params.listingId}`);
+				const data = await res.json();
+				if (data.success === false) {
+					setError(true);
+					setLoading(false);
+					return;
+				}
+				setListing(data);
+				setLoading(false);
+				setError(false);
+			} catch (error) {
+				setError(true);
+				setLoading(false);
+			}
+		};
+		fetchListing();
+	}, [params.listingId]);
+	console.log(loading);
+
+	return <main>
+		{loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+		{error && <p className='text-center my-7 text-2xl'>Something went wrong!</p>}
+	</main>;
+}
