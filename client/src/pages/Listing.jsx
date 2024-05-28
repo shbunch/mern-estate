d545b87ac635185bdd14653ccc5f8748debd2d0d
shbunch <shawnbunch@gmail.com>
2024-05-27 23:58:55 -0500

    Installed Swiper and created slide for listing images

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
 
 export default function Listing() {
+	SwiperCore.use([Navigation]);
 	const [listing, setListing] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
@@ -35,7 +40,22 @@ export default function Listing() {
 			{error && (
 				<p className='text-center my-7 text-2xl'>Something went wrong!</p>
 			)}
-			{listing && !loading && !error && <h1>{listing.name}</h1>}
+			{listing && !loading && !error && (
+				<div>
+					<Swiper navigation>
+						{listing.imageUrls.map((url) => (
+							<SwiperSlide key={url}>
+								<div
+									className='h-[550px]'
+									style={{
+										background: `url(${url}) center no-repeat`, backgroundSize: 'cover'
+									}}
+								></div>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				</div>
+			)}
 		</main>
 	);
 }
