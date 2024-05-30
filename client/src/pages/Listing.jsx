20bedcc3ed91447f58df28bae0c9d2f239707fae
shbunch <shawnbunch@gmail.com>
2024-05-30 00:56:24 -0500

    Created a share icon and function to copy the URL when it is clicked

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -19,6 +19,7 @@ export default function Listing() {
 	const [listing, setListing] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
+	const [copied, setCopied] = useState(false);
 	const params = useParams();
 	useEffect(() => {
 		const fetchListing = async () => {
@@ -64,6 +65,23 @@ export default function Listing() {
 							</SwiperSlide>
 						))}
 					</Swiper>
+					<div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+						<FaShare
+							className='text-slate-500'
+							onClick={() => {
+								navigator.clipboard.writeText(window.location.href);
+								setCopied(true);
+								setTimeout(() => {
+									setCopied(false);
+								}, 2000);
+							}}
+						/>
+					</div>
+					{copied && (
+						<p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+							Link copied!
+						</p>
+					)}
 				</div>
 			)}
 		</main>
