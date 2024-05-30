eebbccd047c15c171268cba1bac04b46b82f0f1f
shbunch <shawnbunch@gmail.com>
2024-05-29 23:56:05 -0500

    Importing Font Awesome Icons to be used on Listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,15 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import {
+	FaBath,
+	FaBed,
+	FaChair,
+	FaMapMarkedAlt,
+	FaMapMarkerAlt,
+	FaParking,
+	FaShare,
+} from 'react-icons/fa';
 
 export default function Listing() {
 	SwiperCore.use([Navigation]);
@@ -48,7 +57,8 @@ export default function Listing() {
 								<div
 									className='h-[550px]'
 									style={{
-										background: `url(${url}) center no-repeat`, backgroundSize: 'cover'
+										background: `url(${url}) center no-repeat`,
+										backgroundSize: 'cover',
 									}}
 								></div>
 							</SwiperSlide>
